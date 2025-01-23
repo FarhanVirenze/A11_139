f2d3ca913a4235c257895662fa1d416f338ab70a
farhan <farhanvirenze18@gmail.com>
2025-01-23 22:26:14 +0700

    membuat PenyediaPengembalianViewModel.kt

---
+++ app/src/main/java/com/pam/pertemuan12/viewmodel/pengembalian/PenyediaPengembalianViewModel.kt
@@ -0,0 +1,26 @@
+package com.pam.pertemuan12.viewmodel.Pengembalian
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.pam.pertemuan12.BukuApplications
+
+object PenyediaPengembalianViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomePengembalianViewModel(aplikasiPengembalian().container.pengembalianRepository) }
+        initializer {
+            InsertPengembalianViewModel(
+                pgn = aplikasiPengembalian().container.pengembalianRepository,
+                bku = aplikasiPengembalian().container.bukuRepository,
+                pjm = aplikasiPengembalian().container.peminjamanRepository
+                // Inject BukuRepository
+            )
+        }
+        initializer { DetailPengembalianViewModel(aplikasiPengembalian().container.pengembalianRepository) }
+        initializer { UpdatePengembalianViewModel(aplikasiPengembalian().container.pengembalianRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiPengembalian(): BukuApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as BukuApplications)
