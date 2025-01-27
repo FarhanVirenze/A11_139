78268fcada55b05cf579fccdfb2119aa81d49f8d
farhan <farhanvirenze18@gmail.com>
2025-01-27 14:02:08 +0700

    fixed PerpustakaanApp.kt

--- app/src/main/java/com/pam/pertemuan12/viewmodel/PerpustakaanApp.kt
+++ app/src/main/java/com/pam/pertemuan12/viewmodel/PerpustakaanApp.kt
@@ -1,11 +1,9 @@
 package com.pam.pertemuan12.viewmodel
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.pam.pertemuan12.navigation.PengelolaHalaman
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PerpustakaanApp(modifier: Modifier = Modifier) {
             PengelolaHalaman()
