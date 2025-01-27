35a3d06bd81f9c6df840610be55a836fbaefd6cc
farhan <farhanvirenze18@gmail.com>
2025-01-27 14:02:19 +0700

    fixed TopAppBar.kt

--- app/src/main/java/com/pam/pertemuan12/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/pam/pertemuan12/costumwidget/TopAppBar.kt
@@ -1,6 +1,7 @@
 package com.pam.pertemuan12.customwidget
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
