bff5054347534ea583803514efc644e523ae16a3
farhan <farhanvirenze18@gmail.com>
2025-01-28 17:23:58 +0700

    fixed HomeAnggotaView.kt

--- app/src/main/java/com/pam/pertemuan12/view/anggota/HomeAnggotaView.kt
+++ app/src/main/java/com/pam/pertemuan12/view/anggota/HomeAnggotaView.kt
@@ -210,12 +210,12 @@ fun AnggotaCard(
     onDeleteClick: (Anggota) -> Unit = {}
 ) {
     Card(
-        modifier = modifier.fillMaxWidth().padding(8.dp),
+        modifier = modifier.fillMaxWidth().padding(2.dp),
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
         Column(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier.padding(6.dp),
             verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
             Row(
